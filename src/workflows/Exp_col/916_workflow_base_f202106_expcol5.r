42dd7348b6be00003da8dceeb8d183b15815a248
alejandro-czernikier <a.czernikier@gmail.com>
2024-11-06 23:12:31 -0300

    Update 916_workflow_base_f202106_expcol5.r

--- src/workflows/Exp_col/916_workflow_base_f202106_expcol5.r
+++ src/workflows/Exp_col/916_workflow_base_f202106_expcol5.r
@@ -272,7 +272,8 @@ TS_strategy_base6 <- function( pinputexps )
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   param_local$final_train$training <- c(202104, 202103, 202102,
-    202101, 202012, 202011, 202010, 202009, 202008, 202007, 202006, 202005, 202004, 202003)
+    202101, 202012, 202011, 202010, 202009, 
+    202008, 202007, 202006, 202005, 202004, 202003)
 
 
   param_local$train$training <- c(202102, 202101, 202012,
