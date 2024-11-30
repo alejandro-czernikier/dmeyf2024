4e0e5af31cdd8d683e7216580ebf2af5ace9e59b
alejandro-czernikier <a.czernikier@gmail.com>
2024-11-30 02:52:12 -0300

    Update 990_workflow_orden227_SEMI_2.r

--- src/workflows/Tercera/990_workflow_orden227_SEMI_2.r
+++ src/workflows/Tercera/990_workflow_orden227_SEMI_2.r
@@ -263,14 +263,14 @@ TS_strategy_base8 <- function( pinputexps )
 
   param_local$meta$script <- "/src/wf-etapas/z2101_TS_training_strategy.r"
 
-  param_local$future <- c(202108)
+  param_local$future <- c(202109)
 
   param_local$final_train$undersampling <- 0.02
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
 
   #Mis meses
   param_local$final_train$training <- c(
-    202106, 202105, #202104,202103,
+    202107, 202106, 202105, #202104,202103,
     202102, 202101, 202012, 202011, 202010,
     202009, 202008, 202007, 
     #202006,
@@ -279,7 +279,7 @@ TS_strategy_base8 <- function( pinputexps )
     201904, 201903)
   
 #mis meses  
-  param_local$train$training <- c(
+  param_local$train$training <- c(202105,
     #202104, 202103, 
     202102, 202101, 202012, 202011, 202010, 202009, 202008,
     202007, 
@@ -289,8 +289,8 @@ TS_strategy_base8 <- function( pinputexps )
     201907, 201906, 201905, 201904, 201908, 201903,
     201902, 201901)
 
-  param_local$train$testing <- c(202106)
-  param_local$train$validation <- c(202105)
+  param_local$train$testing <- c(202107)
+  param_local$train$validation <- c(202106)
 
   # 1.0 significa NO undersampling
   param_local$train$undersampling <- 0.06
