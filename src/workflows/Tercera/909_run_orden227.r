f36770dd6115f8e83773eab75f3b8138c03f8348
alejandro-czernikier <a.czernikier@gmail.com>
2024-11-30 02:51:18 -0300

    Update 909_run_orden227.r

--- src/workflows/Tercera/909_run_orden227.r
+++ src/workflows/Tercera/909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/Semillero/990_workflow_orden227_SEMI.r"
+PARAM <- "src/workflows/Tercera/990_workflow_orden227_SEMI.r"
 
 envg <- env()
 
